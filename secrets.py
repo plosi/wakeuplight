8e2d36bb6b787b4b272fbd73cf6aaaac49f9433e
Paolo Losi <paolo.mizu@gmail.com>
2023-10-16 13:46:19 +0200

    Create secrets.py with dummy data

---
+++ secrets.py
@@ -0,0 +1,13 @@
+# MQTT setup
+mqtt_config = {
+    'client_id': 'name',
+    'broker': 'server_ip_address',
+    'port': 1883,
+    'username': 'my_username',
+    'password': 'my_password',
+    'topic_prefix': 'name',
+    'ssl': True,
+    'ssl_params': {'server_hostname': 'server_ip_address'},
+    'queue_len': 1,
+    'clean': True
+}
